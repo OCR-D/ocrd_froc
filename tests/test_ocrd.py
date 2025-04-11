d6afeda0460b33ece4f7763d85f1f946e4d3704b
Konstantin Baierer <kba@users.noreply.github.com>
2025-04-11 11:53:19 +0200

    tests: use subtests.test instead of plain for loop
    Co-authored-by: Robert Sachunsky <38561704+bertsky@users.noreply.github.com>

--- tests/test_ocrd.py
+++ tests/test_ocrd.py
@@ -22,22 +22,21 @@ def test_froc_defaults(processor_kwargs):
     textstyle0 = textstyle[0]
     assert textstyle0.get('fontFamily') == 'fraktur:100', 'title in fraktur'
 
-def test_froc_ocr(processor_kwargs):
+def test_froc_ocr(processor_kwargs, subtests):
     ws  = processor_kwargs['workspace']
-    # TODO pytest parameterize
     for ocr_method in ['SelOCR', 'adaptive', 'COCR']:
-        file_grp = f'FROC_{ocr_method}'
-        run_processor(
-            FROCProcessor,
-            input_file_grp="OCR-D-SEG-LINE-tesseract-ocropy",
-            output_file_grp=file_grp,
-            parameter={
-                "ocr_method": ocr_method
-            },
-            **processor_kwargs
-        )
-        ws.reload_mets()
-        result_page = page_from_file(next(ws.mets.find_files(ID=f"{file_grp}_0001")))
-        assert result_page.etree.xpath('//page:TextEquiv', namespaces=NAMESPACES), "TextEquivs were created"
-        text0 = result_page.etree.xpath('//page:TextEquiv/page:Unicode/text()', namespaces=NAMESPACES)[0]
-        assert 'erlini' in text0, '"Berlinische Monatsschrift" or similar'
+        file_grp = f'OCR-D-FROC_{ocr_method}'
+        with subtests.test(ocr_method=ocr_method):
+            run_processor(
+                FROCProcessor,
+                input_file_grp="OCR-D-SEG-LINE-tesseract-ocropy",
+                output_file_grp=file_grp,
+                parameter=dict(ocr_method=ocr_method),
+                **processor_kwargs
+            )
+            ws.reload_mets()
+            result_page = page_from_file(next(ws.mets.find_files(ID=f"{file_grp}_0001")))
+            equivs = result_page.etree.xpath('//page:TextEquiv', namespaces=NAMESPACES)
+            assert len(equivs), "TextEquivs were created"
+            text0 = equivs[0].xpath('page:Unicode/text()', namespaces=NAMESPACES)[0]
+            assert 'erlini' in text0, '"Berlinische Monatsschrift" or similar'
